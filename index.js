c1c32fcec7e80a88d58554be1114327fc26a64ba
Nathaniel <nathaniel.wallace@gmail.com>
2022-11-16 10:20:56 -0500

    update headers

--- index.js
+++ index.js
@@ -18,17 +18,17 @@ connectDB()
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization, token"
   )
-  if ('OPTIONS' == req.method){
-    return res.sendStatus(200)
-  } else {
+  // if ('OPTIONS' == req.method){
+  //   return res.sendStatus(200)
+  // } else {
 
     next()
-  }
+  // }
 })
 
 app.use(express.json())
