32f9df4eda32e89506aed97aa43011a00dca25d9
Nathaniel <nathaniel.wallace@gmail.com>
2022-11-16 11:00:03 -0500

    heroku

--- index.js
+++ index.js
@@ -16,17 +16,29 @@ const app = express()
 
 connectDB()
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization, token"
-  )
-  // if ('OPTIONS' == req.method){
-    return res.sendStatus(200)
-  // }
-})
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, token');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // handle OPTIONS method
+  if ('OPTIONS' == req.method) {
+      return res.sendStatus(200);
+  } else {
+      next();
+  }
+});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*")
+//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization, token"
+//   )
+//   // if ('OPTIONS' == req.method){
+//     return res.sendStatus(200)
+//   // }
+// })
 
 app.use(express.json())
 app.use(
