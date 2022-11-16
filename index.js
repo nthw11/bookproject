a7ec7e51d8f4acef70a88c22a282057cc71c42c4
Nathaniel <nathaniel.wallace@gmail.com>
2022-11-16 11:28:49 -0500

    render

--- index.js
+++ index.js
@@ -12,33 +12,21 @@ import messageRoutes from './src/routes/messages.js'
 import loginRoutes from './src/routes/login.js'
 
 const app = express()
-// const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000
 
 connectDB()
 
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, token');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  // handle OPTIONS method
-  if ('OPTIONS' == req.method) {
-      return res.sendStatus(200);
-  } else {
-      next();
-  }
-});
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*")
-//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization, token"
-//   )
-//   // if ('OPTIONS' == req.method){
-//     return res.sendStatus(200)
-//   // }
-// })
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, token"
+  )
+  // if ('OPTIONS' == req.method){
+    return res.sendStatus(200)
+  // }
+})
 
 app.use(express.json())
 app.use(
@@ -55,6 +43,6 @@ app.use("/club", clubRoutes)
 app.use("/board", boardRoutes)
 app.use("/message", messageRoutes)
 
-export default app.listen(process.env.PORT || 8000, () =>{ 
-  // console.log(`app is listening on port ${PORT}`)
+export default app.listen(PORT, () =>{ 
+  console.log(`app is listening on port ${PORT}`)
 })
